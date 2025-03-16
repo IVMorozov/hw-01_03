10caaa0555729886bae7183bbb873eb567f89911
Ilya Morozov <morozov1308@yandex.ru>
2025-03-16 19:59:55 +0300

    send requests

--- hw.py
+++ hw.py
@@ -15,7 +15,17 @@ class  TextRequest:
         """
         метод для отправки текстовых запросов, включая историю
         """
-        pass
+        self.text = text
+        self.model = model     
+        self.client = Mistral(api_key=self.api_key)
+
+        messages = []
+        if history:
+            messages.extend([{'role': msg['role'], 'content': msg['content']} for msg in history])
+        messages.append({'role':'user', 'content': text})
+        chat_response = self.client.chat.complete(model = model, messages = messages)
+        result = {'role': 'assistant', 'content': chat_response.choices[0].message.content}
+        return result
 
 class  ImageRequest:
     """
@@ -24,20 +34,50 @@ class  ImageRequest:
     def __init__(self, api_key: str) -> None:
         self.api_key = api_key
         self.client = Mistral(api_key=api_key)
-        pass
 
     def __encode_image(self, image_path: str) -> str:
         """
         переврд изображения в формат base64
         """
-        pass
+        try:
+            with open(image_path, "rb") as image_file:
+                return base64.b64encode(image_file.read()).decode('utf-8')
+        except FileNotFoundError:
+            print(f"Ошибка: файл {image_path} не найден.")
+            return ""
+        except Exception as e:  # Added general exception handling
+            print(f"Error: {e}")
+            return ""
     
     
     def send(self, text: str, image_path: str, model: str, history: list = None) -> dict:
         """
         метод для отправки текстового запроса совместно с изображением, включая историю
         """
-        pass   
+        base64_image = self.__encode_image(image_path)
+        messages = []
+        if history:
+            messages.extend([{'role': msg['role'], 'content': msg['content']} for msg in history])        
+        messages.append({
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": text
+                },          
+                {
+                    "type": "image_url",
+                    "image_url": f"data:image/jpeg;base64,{base64_image}"
+                }
+            ]
+        } )        
+
+        # Get the chat response
+        chat_response = self.client.chat.complete(model=model, messages=messages)
+
+        # Print the content of the response
+        result = {'role': 'assistant', 'content': chat_response.choices[0].message.content}
+        return result   
 
         
 
